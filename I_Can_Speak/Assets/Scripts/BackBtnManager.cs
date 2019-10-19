cdcdc1184392dc2e04949196e43380138c996fc8
Song_Saens <50483226+ssh9199@users.noreply.github.com>
2019-10-20 01:20:16 +0900

    update: InitView-Base -#2

---
+++ I_Can_Speak/Assets/Scripts/BackBtnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BackBtnManager : MonoBehaviour
+{
+    public GameObject Last_DeActivatedGroup, Now_ActivatedGroup;
+    
+    private void Update()
+    {
+        if (Last_DeActivatedGroup != null && Now_ActivatedGroup != null)
+        {
+            if (OVRInput.GetDown(OVRInput.RawButton.Back)) // 뒤로가기 버튼 -> 상위 메뉴(Last)로 가기
+            {
+                MatchCollection Last__ = Regex.Matches(Last_DeActivatedGroup.name, "_");
+                MatchCollection Now__ = Regex.Matches(Now_ActivatedGroup.name, "_");
+
+                if (Last_DeActivatedGroup != null && Now_ActivatedGroup != null)
+                {
+                    if (Last__.Count < Now__.Count) // _ 의 개수가 적을수록 상위 메뉴다. 즉 Last의 _ 개수가 적으면 실행
+                    {
+                        Last_DeActivatedGroup.SetActive(true);
+                        Now_ActivatedGroup.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+}
