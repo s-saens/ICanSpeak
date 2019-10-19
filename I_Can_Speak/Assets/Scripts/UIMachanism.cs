ca55d59a855b0db0da420cc8e78e41c7f0bd709f
Song_Saens <50483226+ssh9199@users.noreply.github.com>
2019-10-19 18:50:05 +0900

    update: <InitView-Base> -#2 UI상호작용, 시작화면 추가 <시작화면 만드르르르름>

--- I_Can_Speak/Assets/Scripts/UIMachanism.cs
+++ I_Can_Speak/Assets/Scripts/UIMachanism.cs
@@ -1,31 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIMachanism : MonoBehaviour
 {
-    public GameObject PresentationScreen;
-
     #region [ButtonEvent]
-    [SerializeField]
-    public static int Object_Cnt;
-    [SerializeField]
-    public int[] Func_index = new int[Object_Cnt];
-    [SerializeField]
-    public GameObject[] target_Object = new GameObject[Object_Cnt];
+    public string[] Function_name; // 1.Activate, 2.DeActivate, 3.MoveTo~
+    public GameObject[] target_Object;
+    #endregion
+
+    #region [for BackButton]
+    private GameObject Last_DeActivatedGroup;
+    private GameObject Now_ActivatedGroup;
     #endregion
 
     #region [ButtonColor]
     private Color Normal_Color = new Color(1f, 1f, 1f) / 25;
-
-    private Color Button_Now_Color;
+    
     private Color Button_Normal_Color;
     private Color Button_Highlighted_Color;
     private Color Button_Clicked_Color;
 
-    public float Highlight_strength = 40f;
-    public float Click_strength = 50f;
+    private float Highlight_strength = 10f;
+    private float Click_strength = 5f;
     #endregion
 
     #region [Boolean]
@@ -43,13 +42,15 @@ public class UIMachanism : MonoBehaviour
 
     private void Update()
     {
-        Button_Now_Color = this.GetComponent<Renderer>().material.color;
-        this.GetComponent<Renderer>().material.color = Button_Highlighted_Color;
-
+        
         if (CursorIsOn)
         {
-            this.GetComponent<Renderer>().material.color = Button_Clicked_Color;
-            if (this.transform.GetChild(0) != null) this.transform.GetChild(0).gameObject.SetActive(true);
+            this.GetComponent<Renderer>().material.color = Button_Highlighted_Color;
+            if (this.transform.GetChild(0) != null)
+            {
+                Debug.Log("s");
+                this.transform.GetChild(0).gameObject.SetActive(true); // 자식오브젝트 활성화
+            }
 
             if (OVRInput.GetDown(OVRInput.Button.One)) // 딸깍- 버튼누르기시작할 때
             {
@@ -70,23 +71,47 @@ public class UIMachanism : MonoBehaviour
             {
                 this.GetComponent<Renderer>().material.color = Button_Normal_Color;
 
-                ///* 씬 이동버튼 *///
-                if (this.name.Contains("Button_To_"))
+                for (int i = 0; i < Function_name.Length; ++i)
                 {
-                    SceneManager.LoadScene(this.name.Remove(0, 10));
+                    switch (Function_name[i])
+                    {
+                        case "Activate":        //Activate
+                            target_Object[i].SetActive(true);
+                            Now_ActivatedGroup = target_Object[i];
+                            break;
+
+                        case "Deactivate":      //DeActivate
+                            target_Object[i].SetActive(false);
+                            Last_DeActivatedGroup = target_Object[i];
+                            break;
+
+                        case "Move":            //MoveScene
+                            SceneManager.LoadScene(this.name.Remove(0, 10));
+                            break;
+                    }
                 }
-
-
-
                 thisBtnIsStart = false;
             }
 
         }
-
         else if (!thisBtnIsStart && !OVRInput.Get(OVRInput.Button.One))
         {
             this.GetComponent<Renderer>().material.color = Button_Normal_Color;
             if (this.transform.GetChild(0) != null) this.transform.GetChild(0).gameObject.SetActive(false);
+        }
+
+
+        if (OVRInput.GetDown(OVRInput.RawButton.Back)) // 뒤로가기 버튼 -> 상위 메뉴(Last)로 가기
+        {
+            MatchCollection Last__ = Regex.Matches(Last_DeActivatedGroup.name, "_");
+            MatchCollection Now__ = Regex.Matches(Now_ActivatedGroup.name, "_");
+
+            if (Last_DeActivatedGroup != null && Now_ActivatedGroup != null)
+                if (Last__.Count < Now__.Count) // _ 의 개수가 적을수록 상위 메뉴다. 즉 Last의 _ 개수가 적으면 실행
+                {
+                    Last_DeActivatedGroup.SetActive(true);
+                    Now_ActivatedGroup.SetActive(false);
+                }
         }
 
     }
