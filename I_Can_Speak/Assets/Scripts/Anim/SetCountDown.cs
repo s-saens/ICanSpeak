91650d3e76841087eae6d46652fbeb759a237730
Song_Saens <50483226+ssh9199@users.noreply.github.com>
2019-10-21 07:09:29 +0900

    update: 3 Texture

---
+++ I_Can_Speak/Assets/Scripts/Anim/SetCountDown.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCountDown : MonoBehaviour
+{
+    private GameManagerScript GMS;
+
+    public void SetCountDownNow()
+    {
+        GMS = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+        GMS.counterDownDone = true;
+    }
+}
