8b177d1756a953f6fd9197367c15da5c33dffc79
Song_Saens <50483226+ssh9199@users.noreply.github.com>
2019-10-21 00:00:01 +0900

    update: InitView-Base -#2 +맵추가 +용량 최적화 +레이저 버그 수정

--- I_Can_Speak/Assets/Scripts/Presentation.cs
+++ I_Can_Speak/Assets/Scripts/Presentation.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor;
+using TMPro;
 
 public class Presentation : MonoBehaviour
 {
+    public float timeStart = 0f;
+    public GameObject textBox;
+    
     private Object[] textures;
     private int index = 0;
     public bool loaded = false;
     private Vector2 TouchPosition;
+    private GameObject StartHelp;
 
     private void Start()
     {
+        StartHelp = GameObject.Find("STARTG");
         textures = Resources.LoadAll("Presentation", typeof(Texture));
         foreach (var t in textures)
         {
@@ -27,35 +33,35 @@ public class Presentation : MonoBehaviour
 
         if (OVRInput.GetDown(OVRInput.Button.One)) // 터치클릭
         {
-            if (TouchPosition.y < 0.5 && TouchPosition.y >-0.5)
+            if (TouchPosition.y < 0.5 && TouchPosition.y > -0.5)
             {
                 if (TouchPosition.x > 0.5) Next();
                 else if (TouchPosition.x < -0.5) Prev();
-                else FirstLoad();
+                else if (!loaded) FirstLoad();
             }
         }
     }
 
     public void Next()
     {
-        if (index >= textures.Length-1)
+        if (index >= textures.Length - 1)
         {
             if (loaded == true) Debug.Log("This slide is the last one.");
             return;
         }
-        else if(loaded == true)
+        else if (loaded == true)
         {
             index++;
             LOAD(index);
             return;
         }
     }
-    
+
     public void Prev()
     {
         if (index <= 0)
         {
-            if(loaded == true) Debug.Log("This slide is the first one.");
+            if (loaded == true) Debug.Log("This slide is the first one.");
             return;
         }
         else if (loaded == true)
@@ -65,18 +71,20 @@ public class Presentation : MonoBehaviour
             return;
         }
     }
-    
+
     public void FirstLoad()
     {
         index = 0;
         LOAD(0);
         loaded = true;
+        StartHelp.SetActive(false);
         return;
     }
-    
+
     private void LOAD(int ind)
     {
         Texture tex = (Texture)textures[ind];
+        this.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 1);
         this.GetComponent<Renderer>().material.mainTexture = tex;
         return;
     }
